23aa941ef68b62cf21c7c38f3d608b3311c6e56b
mohabashraf <mohabashraf466@gmail.com>
2021-07-15 17:26:56 +0200

    add target hit

--- index.js
+++ index.js
@@ -1,6 +1,29 @@
 const keys = require("./keys");
 const redis = require("redis");
 
+const { Pool } = require("pg");
+const pgClient = new Pool({
+  user: keys.pgUser,
+  host: keys.pgHost,
+  database: keys.pgDatabase,
+  password: keys.pgPassword,
+  port: keys.pgPort,
+});
+
+pgClient.on("connect", (client) => {
+  client
+    .query(
+      "CREATE TABLE IF NOT EXISTS stock_analysis(id SERIAL PRIMARY KEY, stock_id INT, technical_analysis_id INT, target_hit Boolean)"
+    )
+    .catch((err) => console.error(err));
+
+    client
+    .query(
+      "CREATE TABLE IF NOT EXISTS stocks(id SERIAL PRIMARY KEY, stock_id text, name text, price INT, availability INT, timestamp timestamp)"
+    )
+    .catch((err) => console.error(err));});
+
+
 const redisClient = redis.createClient({
   host: keys.redisHost,
   port: keys.redisPort,
@@ -39,6 +62,19 @@ sub.on("message", async (channel, message) => {
     const tech = JSON.parse(technical_analysis);
     const stockData = JSON.parse(stock);
     stockData.technical_analysis = tech;
+    if (stockData.price > target && type === "UP") {
+      stockData.target_hit = true;
+    } else {
+      stockData.target_hit = false;
+    }
+
+    const stock_psql = pgClient.query(
+      "INSERT INTO stocks(stock_id, name, price, availability INT, timestamp timestamp) VALUES($1, $2, $3, $4)",
+      [stock.stock_id, stock.name, stock.price, stock.availability, stock.timestamp]
+    );
+
+    console.log("The stock from psql" + stock_psql);
+    
     redisClient.hset("stocks_analysis", message, JSON.stringify(stockData));
   }
 });
